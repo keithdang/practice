de69d15be6f4903e27f0c60f2a2bc3ee96332ef4
keith <kdang@quartermaster.house>
2018-11-02 16:24:39 -0400

    commit to feature branch

--- Main.java
+++ Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
+        int total=addition(5,4);
+        System.out.println("Total:"+total);
+    }
+    public static int addition(int a,int b){
+        return a+b;
     }
 }
